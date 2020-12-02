0711f6cf308e99a6d62879346b339598f808927a
EzequielCaste <ezequiel.caste@gmail.com>
2020-12-02 19:13:28 -0300

    Mobile and Desktop design

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-const shareBtn = document.querySelector('.share-btn');
-const avatarImg = document.querySelector('#avatar')
-const nameDiv = document.querySelector('.name')
-const shareContent = document.querySelector('.share-content')
+const shareBtn = document.querySelectorAll('.share-btn');
 
-shareBtn.addEventListener('click', () => {
 
- avatarImg.classList.toggle('hide')
- nameDiv.classList.toggle('hide')
- shareContent.classList.toggle('hide')
+console.log(shareBtn);
+
+shareBtn.forEach(el => el.addEventListener('click', () => {
+
+
+  if( window.innerWidth > 1020 ) {
+    //show desktop links
+    const avatarImg = document.querySelector('#avatar-desktop')
+    const nameDiv = document.querySelector('.name-desktop')
+    const shareContent = document.querySelector('.share-content-desktop')
+    // avatarImg.classList.toggle('hide')
+    // nameDiv.classList.toggle('hide')
+    shareContent.classList.toggle('hide')
+  } else {
+    const avatarImgMobile = document.querySelector('#avatar-mobile')
+    const nameDivMobile = document.querySelector('.name-mobile')
+    const shareContentMobile = document.querySelector('.share-content-mobile')
+    avatarImgMobile.classList.toggle('hide')
+    nameDivMobile.classList.toggle('hide')
+    shareContentMobile.classList.toggle('hide')
+  }
 })
+)
